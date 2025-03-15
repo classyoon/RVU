cc6a8624ccc94164ca64a0b90cf238e83224b769
classyoon <96508736+classyoon@users.noreply.github.com>
2025-03-15 12:38:08 -0400

    Initial Commit

---
+++ RVU/RVUApp.swift
@@ -0,0 +1,17 @@
+//
+//  RVUApp.swift
+//  RVU
+//
+//  Created by Conner Yoon on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct RVUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RVUTests/RVUTests.swift
@@ -0,0 +1,17 @@
+//
+//  RVUTests.swift
+//  RVUTests
+//
+//  Created by Conner Yoon on 3/15/25.
+//
+
+import Testing
+@testable import RVU
+
+struct RVUTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
